3873ef206a61304acb8083b90aca61700e6cfe39
Katie <61799774+katiepdx@users.noreply.github.com>
2021-07-17 15:30:03 -0700

    add fetch interval

--- App.js
+++ App.js
@@ -4,34 +4,42 @@ import { addNewTime } from './utils.js';
 // CREATE COMPONENT
 const App = () => {
     const [timesArray, setTimesArray] = React.useState([])
+    const [stopToggle, setStopToggle] = React.useState(false)
 
     let fetchStart;
     let fetchEnd;
     let responseTime;
 
-    const handleClick = () => {
+    // SET TIME INTERVAL - makes fetch every second
+    const fetchInterval = setTimeout(() => {      
       fetchStart = Date.now()
-
-      // fetch on click
+      
       fetch('http://localhost:3000/', { mode: 'no-cors'})
         .then(data => console.log('api res: ', data))
-        // calc fetch time
         .then(() => {
+          // calc fetch time
           fetchEnd = Date.now()
           responseTime = (fetchEnd - fetchStart)
-
+          
           const updatedArr = addNewTime(timesArray, responseTime)
           
           setTimesArray(updatedArr)
-        })
-
-    }
+        });
+  
+    }, 1000)
+    
+    // START/STOP fetch interval
+    if(stopToggle === false) fetchInterval
+    if(stopToggle === true) clearInterval(fetchInterval) 
+    
+    // TOGGLE start/top
+    const handleClick = () => setStopToggle(!stopToggle)
 
     return (
       React.createElement('div', null, [
         React.createElement('h1', {key: 'h1'}, 'Response Times'),
         React.createElement('div', {key: 'chart-data'}, `Fetch Times Array: ${timesArray}`),
-        React.createElement('button', {key: 'fetch-btn', onClick: handleClick}, 'Click')
+        React.createElement('button', {key: 'stop-btn', onClick: handleClick}, 'Start/Stop')
       ])
     )
 }
